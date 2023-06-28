2b1ed48666da14ffedfd72a1085403b5720afb04
syn <1599386309@qq.com>
2023-06-29 01:50:59 +0800

    asddd

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
-// import App from './App.vue'
-import Composition from './Composition.vue'
-import TeleportTest from './TeleportTest.vue'
+import App from './App.vue'
+// import Composition from './Composition.vue'
+// import TeleportTest from './TeleportTest.vue'
 
-// const app = createApp(App)
-// app.mount('#app')
+const app = createApp(App)
+app.mount('#app')
 
-const composition = createApp(Composition)
-composition.mount('#composition')
+// const composition = createApp(Composition)
+// composition.mount('#composition')
 
 
-const teleportTest = createApp(TeleportTest)
-teleportTest.mount('#teleportTest')
+// const teleportTest = createApp(TeleportTest)
+// teleportTest.mount('#teleportTest')
